957e6d653b09294926b351cc690f6a4a1ba74441
Greta Moseley <greta.moseley@gmail.com>
2017-08-09 15:42:19 -0500

    add topic_id to ng-click function in topics partial, modify faves controller to get access to the video object

--- app/controllers/faves-controller.js
+++ app/controllers/faves-controller.js
@@ -4,24 +4,26 @@ latinApp.controller("FavesController", function($scope, $window, UserFactory, Vi
 //how to add property of faves [] to user object in firebase if it doesn't exist
 //how to add to that array?
 //how to do a patch?
-    $scope.postVideoToUserFaves=(videoId)=>{//fires on click of add to favorites button on video
+    $scope.postVideoToUserFaves=(videoId, topicId)=>{//fires on click of add to favorites button on video
         console.log ("postvideofaves fired");
         console.log ("event", videoId);
+        console.log ("topicId",topicId);
+        VideoFactory.getOneTopic(topicId)
+        .then((topic)=>{
+            //reduce that topic object to just the video object that matches the id of the card clicked
+            console.log ("topic in postVideoToUserFaves", topic);
+            let vidObj = topic[0].videos.reduce(function(prev,curr){
+                return curr.video_id === videoId ? curr:prev;}, null);
+            console.log ("result", vidObj);
+            //getUser, get user obj
 
+            //userObj.faves = [vidObj]?
 
-        // VideoFactory.getOneTopic(topicId)
-        // .then((topic)=>{
-        //     //reduce that topic object to just the video object that matches the id of the card clicked
-        //     console.log ("topic in postVideoToUserFaves", topic);
-            // let vidObj = topic[0].videos.reduce(function(prev,curr){
-            //     return curr.video_id === videoId ? curr:prev;}, null);
-            // console.log ("result", vidObj);
-
-            //userObj.faves = vidObj
-
-            //put that back on fb
+            //put that back on fb to replace old user object
 
             //put that videoObject on userObject in FB - if no faves property exists, add property and push obj into that array else just push object into faves array
-
+        });
     };
+
+
 });
