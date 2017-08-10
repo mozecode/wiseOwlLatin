6a76dd3895273808a6b47ca129b17730a59b6eb5
Greta Moseley <greta.moseley@gmail.com>
2017-08-10 10:56:04 -0500

    modify faves.html, navbar.html, and faves controller to get user favorites to appear based on the video object stored on the user object

--- app/controllers/faves-controller.js
+++ app/controllers/faves-controller.js
@@ -1,43 +1,45 @@
 'use strict';
 latinApp.controller("FavesController", function($scope, $window, UserFactory, VideoFactory){
 
-//how to add property of faves [] to user object in firebase if it doesn't exist
-//how to add to that array?
-//how to do a patch?
     $scope.postVideoToUserFaves=(videoId, topicId)=>{//fires on click of add to favorites button on video
-        console.log ("postvideofaves fired");
-        console.log ("event", videoId);
-        console.log ("topicId",topicId);
         VideoFactory.getOneTopic(topicId)
         .then((topic)=>{
             //reduce that topic object to just the video object that matches the id of the card clicked
-             console.log ("topic in postVideoToUserFaves", topic);
-             let topicKey = Object.keys(topic);
-             console.log ("topicKey",topicKey);
-
+            let topicKey = Object.keys(topic);
             let vidObj = topic[topicKey].videos.reduce(function(prev,curr){
                 return curr.video_id === videoId ? curr:prev;}, null);
-
-            console.log ("result", vidObj);
+            //get userObject
             let currentUser = UserFactory.getUser();
             UserFactory.userCheck(currentUser)
             .then((userObj)=>{
                 console.log ("vidObjinside.then",vidObj);
                 console.log ("userObjinsidefaves",userObj);
                 let userKey= Object.keys(userObj);
+                //get key of user object and use to do a patch to replace original value of faves on the user object with an array of objects as user adds faves
                     if (userObj[userKey].faves === 0){
                     userObj[userKey].faves = [];
-                }
+                    }
                     userObj[userKey].faves.push(vidObj);
-                 //put that videoObject on userObject - if no faves property exists, add property and push obj into that array else just push object into faves array
                 UserFactory.patchUpdatedUserOnFB(userObj[userKey], userKey)
                 .then((newUserData)=>{
                     console.log ("newUserData",newUserData);
                 });
             });
-            //put that back on fb to replace old user object
         });
     };
 
+    let getUserFaves=()=>{
+        let currentUser = UserFactory.getUser();
+        UserFactory.userCheck(currentUser)
+        .then((userObj)=>{
+            console.log("userforfavescheck", userObj);
+            let userKey= Object.keys(userObj);
+            console.log ("userKeyforFavesCheck", userKey);
+            $scope.faves = userObj[userKey].faves;
+            console.log ("scope.faves",$scope.faves);
+        });
 
+    };
+
+    getUserFaves();
 });
