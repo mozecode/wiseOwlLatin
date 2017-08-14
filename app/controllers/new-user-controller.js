4e3a0a1fcf4762d5c68d374a5181ea271b5a7a6b
Greta Moseley <greta.moseley@gmail.com>
2017-08-14 12:53:36 -0500

    add user name and avatar to topics and faves pages

--- app/controllers/new-user-controller.js
+++ app/controllers/new-user-controller.js
@@ -7,11 +7,8 @@ latinApp.controller("NewUserController", function($scope, $window, UserFactory, 
     //get user object
     UserFactory.userCheck(currentUser)
     .then((userObj)=>{
-        console.log ("userObjnewUser", userObj);
         let userKey = Object.keys(userObj);
-        console.log ("userKey",userKey);
         $scope.userObject = userObj[userKey];
-        console.log ("userObject on scope", $scope.userObject);
     });
     //use info from user object to populate the new user welcome partial by putting the user object on scope
 
